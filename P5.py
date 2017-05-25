27e3eb91cde5a8eeca5bb0f489341c9ca6ee9e90
watszn <nawatson13@gmail.com>
2017-05-24 20:48:23 -0500

    solution for P5

---
+++ P5.py
@@ -0,0 +1,32 @@
+# project euler P5
+# smallest multiple that is evenly divisible by numbers 1-20
+
+# 2, 4, 6, 8, 10, 12, 14, 16, 18
+# 3, 9
+# 5, 15, 20
+# 7, 11, 13, 17, 19
+
+def smallest_multiple(a, b): # a = list; b = num
+    div_evenly = True
+    for num in a:
+        if b % num != 0:
+            div_evenly = False
+            break
+    return div_evenly
+
+
+
+def main():
+    # create list 1 - 20
+    num_list = []
+    for num in range (1, 21):
+        num_list.append(num)
+    print (num_list)
+
+    num = 1
+    while (smallest_multiple(num_list, num) == False):
+        num += 1
+    print (num)
+
+
+main()
